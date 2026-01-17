23904a339e3195b6ed65481901a21b0548924adb
Bright Borngreat <borngreatbright019@gmail.com>
2026-01-17 19:00:14 +0000

    PWA Elements upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'zenbudget-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  './logo-192x192.png',
+  './logo-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
