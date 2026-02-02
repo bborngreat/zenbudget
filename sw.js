917b8f47c5a44a85431994be13e90485fb2c0b30
Bright Borngreat <borngreatbright019@gmail.com>
2026-02-02 01:00:27 +0000

    Update service worker cache version to v1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zenbudget-v1';
+const CACHE_NAME = 'zenbudget-v1.2';
 const assets = [
   './index.html',
   './manifest.json',
@@ -18,3 +18,4 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
+
